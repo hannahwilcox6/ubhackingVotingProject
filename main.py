08cd12f72c851d9a1cd80954387442b25321789c
brodiele <brodiele@buffalo.edu>
2020-10-24 18:17:25 -0400

    Added player class Integrated interaction code into player class

--- main.py
+++ main.py
@@ -1,58 +1,66 @@
 import pygame
-
 pygame.init()
 
 #  game window, with height and width
-screen = pygame.display.set_mode((1500, 600))
-# Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+screen = pygame.display.set_mode((1500,600))
+#Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
 pygame.display.set_caption("UBHacking Voting 2020")
 icon = pygame.image.load("vote.png")
 pygame.display.set_icon(icon)
+
 running = True
 
-# Player
-playerImg = pygame.image.load("standing-up-man-.png")
-playerX = 370
-playerY = 480
-playerMove = 0
+class Player:
+    playerX_change = 0
+    playerY_change = 0
+    quizScreen = False
 
+    def __init__(self, img, x, y):
+        self.playerImg = pygame.image.load(img)
+        self.playerX = x
+        self.playerY = y
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
-
-def quizBackground():
-    rect = pygame.Surface((1000, 750))  # the size of your rect
-    rect.set_alpha(128)  # alpha level
-    rect.fill((255, 255, 255))
-    screen.blit(rect,(0,0))
-
-quizScreen = False
-
-
-while(running == True):
-    screen.fill((128, 242, 233))
-    # Keeping track of the state of the window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: #If it closes, we quit.
-            running = False
-        # User interaction events
+    def actions(self):
+        if player.playerX <= 0:
+            player.playerX = 0
+        elif player.playerX >= 1436:
+            player.playerX = 1436
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a: #Left
-                playerMove = -.2
-            if event.key == pygame.K_d: #Right
-                playerMove = .2
-            if event.key == pygame.K_e: #AND collision
-                quizScreen = True
+            if event.key == pygame.K_LEFT:
+                self.playerX_change -= 0.2
+            if event.key == pygame.K_RIGHT:
+                self.playerX_change += 0.2
+            if event.key == pygame.K_e:
+                self.quizScreen = True
+                #player.quizBackground()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or pygame.K_RIGHT:
+                self.playerX_change = 0
 
-        if event.type == pygame.KEYUP: #User interaction ends.
-            if event.key == pygame.K_a or event.key == pygame.K_d:
-                playerMove = 0
-    playerX += playerMove
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= 1436:
-        playerX = 1436
-    player()
-    if(quizScreen == True):
-        quizBackground()
+    def player(self):
+        screen.blit(self.playerImg, (self.playerX, self.playerY))
+        self.playerX += self.playerX_change
+        self.playerY += self.playerY_change
+
+    def quizBackground(self):
+        if self.quizScreen:
+            rect = pygame.Surface((1000, 750))  # the size of your rect
+            rect.set_alpha(128)  # alpha level
+            rect.fill((255, 255, 255))
+            screen.blit(rect, (0, 0))
+
+
+
+player = Player("standing-up-man-.png", 370, 480)
+while running:
+    screen.fill((128, 242, 233))
+    # Window is closed.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    #Checks if player is hitting buttons
+    player.actions()
+
+    player.player()
+    player.quizBackground()
     pygame.display.update()
