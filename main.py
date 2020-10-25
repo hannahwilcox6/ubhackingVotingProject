ff7cea695f573f8cbbcf803a4427ce141c8e9904
hnh <hannahwilcox6@gmail.com>
2020-10-25 00:39:25 -0400

    MORE QUIZZES

--- main.py
+++ main.py
@@ -19,9 +19,14 @@ quiz1 = {"When is the deadline to register to vote?": ["[A] November 3", "[B] Oc
                                                              "[C] Various websites online",
                                                              "[D] Just remember when you did it"]}
 ans1 = ["B","A","C"]
-test1 = {"How old do you need to be to vote?": ["[A] 18", "[B] 21", "[C] 28", "[D] 16"]}
-
-
+quiz2 = {"Who isn't a current official presidential candidate?": ["[A] Joe Biden - Kamala Harris", "[B] Donald Trump - Mike Pence", "[C] Jo Jorgenson - Spike Cohen", "[D] Daniel Rotter - Grace Grenki"],
+         "Who can you write in for the presidential candidate?": ["[A] Kanye West", "[B] Your mom", "[C] Your school principal", "[D] All of the above"],
+         "How should you research candidates?": ["[A] Don't, just pick randomly.","[B] Only look from one news site", "[C] Check a variety of sources with different views", "[D] Ask your parents"]}
+ans2 = ["D","D","C"]
+quiz3 = {"When does early voting occur in NYS?": ["[A] It doesn't","[B] October 2 - November 3", "[C] October 24 - November 3","[D] None of the above"],
+         "How can you request a mail in ballot?": ["[A] Following the steps in websites like voteearlyny.org","[B] Through your local election office","[C] Going to vote.org and filling out the application online", "[D] All of the above"],
+         "When is election day?": ["[A] November 3", "[B] 6AM - 9PM", "[C] During designated early voting periods", "[D] All of the above"]}
+ans3 = ["C","D","D"]
 
 Y = 460
 npc1 = npc.NPC("man1.png",690, Y)
@@ -47,11 +52,6 @@ while running:
         else:
             p1.actions(event)
     #Checks if player is hitting buttons
-    if(q1.quizActive):
-        q1.draw()
-    else:
-        p1.quizScreen = False
-        p1.interacting = False
     for i in npcs:
         i.draw(screen)
     p1.collision(npc1)
@@ -59,5 +59,9 @@ while running:
     p1.draw(screen)
     if(q1.quizActive):
         q1.draw()
+    else:
+        p1.quizScreen = False
+        p1.interacting = False
+
 
     pygame.display.update()
